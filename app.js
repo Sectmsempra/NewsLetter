c290bf7656d062df9c219caa2680ea8f0f733674
Sectmsempra <poojayog02@gmail.com>
2023-06-04 11:23:32 +0530

    Complete Project

--- app.js
+++ app.js
@@ -1,35 +1,69 @@
 //jshint esversion: 6
-const express = require('express');
-const https = require('https');
-const bodyParser = require('body-parser');
-const request = require('request');
-
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
-// app.use(express.static(__dirname + '/public'));
+app.post("/", function(req, res) {
 
-app.get("/", function(req, res){
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
 
-    res.sendFile(__dirname + "/signup.html")
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/4946c06aa9";
+
+    const options = {
+        method: "POST", // here
+        auth: "pooja1502: df18c6663b6b2c8c78a0a13f3f7128b1-us21"
+    }
+
+
+
+    const request = https.request(url, options, function(response) {
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+
+    })
+
+    request.write(jsonData);
+    request.end();
+
+
+
+
 
 });
 
-app.post("/", function(req, res){
-
-    var firstname = req.body.fname;
-    var lastname = req.body.lname;
-    var email = req.body.email;
-
-    console.log(firstname, lastname, email);
-
-});
-
-
-app.listen("3000", function(){
-    console.log("Server is running on 3000")
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000");
 });
